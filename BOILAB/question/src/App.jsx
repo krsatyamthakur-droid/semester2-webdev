5514536080a74d6c55e25ddcd9fc74eecb760102
satyam kumar <krsatyamthakur@gmail.com>
2026-04-18 23:19:37 +0530

    classes

--- BOILAB/question/src/App.jsx
+++ BOILAB/question/src/App.jsx
@@ -1,17 +1,26 @@
-import React, { useEffect } from 'react'
-import Temperature from './temperature.jsx';
-import ChildComponents from './components/ChildComponents.jsx';
-import ParentComponents from './components/ParentComponents.jsx';
-const App = () => {
+// import React, { useEffect } from 'react'
+// import Temperature from './temperature.jsx';
+// import ChildComponents from './components/ChildComponents.jsx';
+// import ParentComponents from './components/ParentComponents.jsx';
+// const App = () => {
 
+//   return (
+//     <div>
+//       {/* <Temperature /> */}
+//       <ChildComponents />
+//       <ParentComponents />
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React from 'react'
+
+const App = () => {
   return (
-    <div>
-      {/* <Temperature /> */}
-      <ChildComponents />
-      <ParentComponents />
-    </div>
+    <div>App</div>
   )
 }
 
-export default App;
-
+export default App
